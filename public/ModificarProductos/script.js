94a4879e9593f5e4ebf30dd7136bd35f30b6a8e0
Federico Storti <34277220+fedestorti@users.noreply.github.com>
2025-06-18 14:32:17 -0300

    Update script.js

--- public/ModificarProductos/script.js
+++ public/ModificarProductos/script.js
@@ -91,7 +91,7 @@ window.prepararEdicion = codigo => {
   const prod = productos.find(p => p.codigo_producto === codigo);
   if (!prod) return;
 
-  codigoActual = prod.codigo_producto;
+  codigoActual = prod.codigo;
   editNombre.value = prod.nombre_producto;
   editPrecio.value = prod.precio;
   previewEditar.innerHTML = `<img src="${prod.imagen}" />`;
